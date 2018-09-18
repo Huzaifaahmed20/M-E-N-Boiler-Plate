d187127147d92ff133d4da7c38757d1d37ba7a1e
Huzaifa Ahmed <ahmedhuzaifa13@gmail.com>
2018-09-18 17:52:08 +0500

    added auth functions .. added headers for CRUD Operations

--- routes.js
+++ routes.js
@@ -7,7 +7,18 @@ export default function (app) {
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
     app.use(morgan('dev'));
-
+    
+    app.use(function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Cache-Control, Authorization');
+        if (req.method === 'OPTIONS') {
+        res.statusCode = 204;
+        return res.end();
+        } else {
+        return next();
+        }
+    });
 
     app.use("/api/product", require("./server/api/product"))
     app.use("/api/user", require("./server/api/user"))
--- server/api/auth/auth.service.js
+++ server/api/auth/auth.service.js
@@ -1,13 +1,65 @@
 import config from '../../config/';
 import jwt from 'jsonwebtoken';
 import expressJwt from 'express-jwt';
-// import compose from 'composable-middleware';
-// import User from '../api/user/user.model';
+import compose from 'composable-middleware';
+import User from '../api/user/user.model';
+var validateJwt = expressJwt({
+    secret: config.mySecret
+});
 
-// var validateJwt = expressJwt({
-//     secret: config.secrets.session
-// });
 
+/**
+ * Attaches the user object to the request if authenticated
+ * Otherwise returns 403
+ */
+export function isAuthenticated() {
+    return compose()
+        // Validate jwt
+        .use(function(req, res, next) {
+            // console.log(req)
+            // allow access_token to be passed through query parameter as well
+            if(req.query && req.query.hasOwnProperty('access_token')) {
+                req.headers.authorization = `Bearer ${req.query.access_token}`;
+            }
+            // IE11 forgets to set Authorization header sometimes. Pull from cookie instead.
+            if(req.query && typeof req.headers.authorization === 'undefined') {
+                req.headers.authorization = `Bearer ${req.cookies.token}`;
+            }
+            validateJwt(req, res, next);
+        })
+        // Attach user to request
+        .use(function(req, res, next) {
+            User.findById(req.user._id).exec()
+                .then(user => {
+                    if(!user) {
+                        return res.status(401).end();
+                    }
+                    req.user = user;
+                    next();
+                    return null;
+                })
+                .catch(err => next(err));
+        });
+}
+
+/**
+ * Checks if the user role meets the minimum requirements of the route
+ */
+export function hasRole(roleRequired) {
+    if(!roleRequired) {
+        throw new Error('Required role needs to be set');
+    }
+
+    return compose()
+        .use(isAuthenticated())
+        .use(function meetsRequirements(req, res, next) {
+            if(config.userRoles.indexOf(req.user.role) === config.userRoles.indexOf(roleRequired)) {
+                return next();
+            } else {
+                return res.status(403).send('Forbidden');
+            }
+        });
+}
 
 
 
