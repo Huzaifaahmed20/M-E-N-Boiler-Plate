c2274368ec5716298f778da48938f7f2d33b0ec2
Huzaifa Ahmed <ahmedhuzaifa13@gmail.com>
2018-10-02 16:50:38 +0500

    minor bug fixed

--- server/api/auth/auth.service.js
+++ server/api/auth/auth.service.js
@@ -2,7 +2,7 @@ import config from '../../config/';
 import jwt from 'jsonwebtoken';
 import expressJwt from 'express-jwt';
 import compose from 'composable-middleware';
-import User from '../api/user/user.model';
+import User from '../../api/user/user.model';
 var validateJwt = expressJwt({
     secret: config.mySecret
 });
